d15a2fdf7daf828ece41109354415396fb9f1194
ale1934 <alesees10@gmail.com>
2026-02-08 06:42:50 -0600

    cleaned up

--- build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/ale/Programming/editor/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/ale/Programming/editor/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
